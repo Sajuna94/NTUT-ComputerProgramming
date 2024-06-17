d0ea36593248a9e23b56292d8392c540ce650f0c
Sajuna94 <122368633+Sajuna94@users.noreply.github.com>
2024-06-17 15:57:46 +0800

    Update weight.c

--- CHESS/Sources/weight.c
+++ CHESS/Sources/weight.c
@@ -1,8 +1,14 @@
 #include "../Headers/weight.h"
 
-int calcLiveWeight(Chess* chess, Piece piece, Point position) {
-    bool DIE_3 = false, DIE_4 = false;
-    bool LIVE_3 = false, LIVE_4 = false;
+int calcLiveWeight(Chess *chess, Piece piece, Point position)
+{
+    bool LIVE_5 = false;
+    bool LIVE_4 = false, DOUBLE_DIE_4 = false, DIE_4_LIVE_3 = false;
+    bool DOUBLE_LIVE_3 = false, DIE_3_LIVE_3_HIGH = false;
+    bool DIE_4 = false, LOW_DIE_4 = false;
+    bool LIVE_3 = false, JUMP_LIVE_3 = false;
+    bool DOUBLE_LIVE_2 = false, LIVE_2 = false, LOW_LIVE_2 = false;
+    bool DIE_3 = false, DIE_2 = false;
 
     for (int i = 0; i < 8; i += 2) {
         LiveHelper liveA = { .length = 0, .IS_LIVE = false };
@@ -13,46 +19,69 @@ int calcLiveWeight(Chess* chess, Piece piece, Point position) {
 
         LiveHelper liveResult = { .length = liveA.length + liveB.length, .IS_LIVE = liveA.IS_LIVE && liveB.IS_LIVE };
 
-        // 可能造成長連?
-        if (piece == BLACK && liveResult.length > 3) {
-            return 1; // 長連對黑旗沒有權值效益?
-        }
-        else if (liveResult.length >= 3) {
-            if (liveResult.IS_LIVE)
+        if (liveResult.length >= 4 && liveResult.IS_LIVE) {
+            LIVE_5 = true;
+        } else if (liveResult.length == 4) {
+            if (liveResult.IS_LIVE) {
                 LIVE_4 = true;
-            if (liveA.IS_LIVE || liveB.IS_LIVE) // 被圍住的會不算die4
+            } else if (liveA.IS_LIVE || liveB.IS_LIVE) {
                 DIE_4 = true;
-        }
-        else if (liveResult.length == 2) {
-            if (liveResult.IS_LIVE)
-                LIVE_3 = true;
-            if (liveA.IS_LIVE || liveB.IS_LIVE)
+            } else {
+                LOW_DIE_4 = true;
+            }
+        } else if (liveResult.length == 3) {
+            if (liveResult.IS_LIVE) {
+                if (liveA.length == 3 || liveB.length == 3) {
+                    JUMP_LIVE_3 = true;
+                } else {
+                    LIVE_3 = true;
+                }
+            } else if (liveA.IS_LIVE || liveB.IS_LIVE) {
+                DIE_3_LIVE_3_HIGH = true;
+            } else {
                 DIE_3 = true;
+            }
+        } else if (liveResult.length == 2) {
+            if (liveResult.IS_LIVE) {
+                LIVE_2 = true;
+            } else if (liveA.IS_LIVE || liveB.IS_LIVE) {
+                LOW_LIVE_2 = true;
+            } else {
+                DIE_2 = true;
+            }
         }
     }
 
-    int total = 1, combo = 1;
+    int total = 1;
 
-    if (LIVE_4) { printf("live4, "); total += 20 * combo++; }
-    if (LIVE_3) { printf("live3, ");total +=  10 * combo++; }
-    if (DIE_4) { total += 2 * combo++; }
-    if (DIE_3) { total += 1 * combo++; }
-    
+    if (LIVE_5) total = 14;
+    else if (LIVE_4 || (DIE_4 && LIVE_3) || DOUBLE_DIE_4) total = 13;
+    else if (DOUBLE_LIVE_3) total = 12;
+    else if (DIE_3_LIVE_3_HIGH) total = 11;
+    else if (DIE_4) total = 10;
+    else if (LOW_DIE_4) total = 9;
+    else if (LIVE_3) total = 8;
+    else if (JUMP_LIVE_3) total = 7;
+    else if (DOUBLE_LIVE_2) total = 6;
+    else if (LIVE_2) total = 5;
+    else if (LOW_LIVE_2) total = 4;
+    else if (DIE_3) total = 3;
+    else if (DIE_2) total = 2;
+
     return total;
 }
 
-LiveHelper* LiveDFS(Chess* chess, Piece piece, Point dirPt, int x, int y, LiveHelper* liveHelper) {
+LiveHelper *LiveDFS(Chess *chess, Piece piece, Point dirPt, int x, int y, LiveHelper *liveHelper)
+{
     if (!isSafe(x, y))
         return liveHelper;
 
-    if (chess->board[y][x] != piece) {
-        // 最後一格為空
+    if (chess->board[y][x] != piece)
+    {
         if (chess->board[y][x] == EMPTY)
             liveHelper->IS_LIVE = true;
-
         return liveHelper;
     }
     liveHelper->length++;
-
     return LiveDFS(chess, piece, dirPt, x + dirPt.X, y + dirPt.Y, liveHelper);
 }
